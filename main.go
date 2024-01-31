20da579c197f5e0194bb0e27944f57d119df408c
Nanthachai.CS07 <botsafe002@gmail.com>
2024-01-31 12:24:18 +0700

    Add basicTest whit grom

--- main.go
+++ main.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "myuser"
+	password = "mypassword"
+	dbname   = "mydatabase"
+)
+
+type User struct {
+	gorm.Model
+	Fullname string
+	Email    string `gorm:"unique"`
+	Age      int
+}
+
+func InitializeDB() *gorm.DB {
+	// Configure your PostgreSQL database details here
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	db.AutoMigrate(&User{})
+	return db
+}
+
+func AddUser(db *gorm.DB, fullname, email string, age int) error {
+	user := User{Fullname: fullname, Email: email, Age: age}
+
+	var count int64
+	db.Model(&User{}).Where("email = ?", email).Count(&count)
+	if count > 0 {
+		return errors.New("email already exists")
+	}
+
+	result := db.Create(&user)
+	return result.Error
+}
+
+func main() {
+	db := InitializeDB()
+
+	AddUser(db, "John Doe", "jane.doe@example.com", 30)
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,60 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+const (
+	host     = "localhost"  // or the Docker service name if running in another container
+	port     = 5432         // default PostgreSQL port
+	user     = "myuser"     // as defined in docker-compose.yml
+	password = "mypassword" // as defined in docker-compose.yml
+	dbname   = "mydatabase" // as defined in docker-compose.yml
+)
+
+type User struct {
+	gorm.Model
+	Fullname string
+	Email    string `gorm:"unique"`
+	Age      int
+}
+
+// InitializeDB initializes the database and automigrates the User model.
+func InitializeDB() *gorm.DB {
+	// Configure your PostgreSQL database details here
+	dsn := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	db.AutoMigrate(&User{})
+	return db
+}
+
+// AddUser adds a new user to the database.
+func AddUser(db *gorm.DB, fullname, email string, age int) error {
+	user := User{Fullname: fullname, Email: email, Age: age}
+
+	// Check if email already exists
+	var count int64
+	db.Model(&User{}).Where("email = ?", email).Count(&count)
+	if count > 0 {
+		return errors.New("email already exists")
+	}
+
+	// Save the new user
+	result := db.Create(&user)
+	return result.Error
+}
+
+func main() {
+	db := InitializeDB()
+	// Your application code
+	AddUser(db, "John Doe", "jane.doe@example.com", 30)
+}
